ef5afda946bdc79f09b13b9447b6d1fd8f32e900
hellowhirl <hellowhirl@users.noreply.github.com>
2019-05-01 23:32:32 +0900

    include sample recipes and mapping

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import RecipeCard from './components/RecipeCard';
+import { recipes } from './recipes.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <RecipeCard />
-
+        {
+          recipes.map(m =>
+            <RecipeCard
+              name={m.name}
+              ingredients={m.ingredients}
+             />
+          )
+        }
       </div>
     );
   }
